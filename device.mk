66fee367f76253b03faf7d2c8d926dc8d8a8079a
ManoloRey <cleitonmca@gmail.com>
2018-06-08 10:11:03 +0000

    vince: build with Snap

--- device.mk
+++ device.mk
@@ -133,7 +133,7 @@ PRODUCT_PACKAGES += \
     camera.device@3.2-impl \
     camera.msm8953 \
     libmm-qcamera \
-    Camera2 \
+    Snap \
     GCam \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
