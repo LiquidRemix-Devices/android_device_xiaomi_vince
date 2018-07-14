eb2a26a962afc803c819d812b2c24e11e0419aa2
wendelneves7 <wendelneves7@gmail.com>
2018-07-14 21:49:49 +0000

    Add Clock

--- device.mk
+++ device.mk
@@ -144,6 +144,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-service
 
+# DeskClock
+PRODUCT_PACKAGES += \
+   DeskClock 
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
